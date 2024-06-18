864594afec4c0d6e3fe60b4f3e6332952abd1d58
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-19 00:13:24 +0500

    update
    continue

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let change = "";
 function Generate(){
     let input = document.getElementById("input");
     change = '';
-   for (let i = 0; i <= 3; i++) {
+   for (let i = 0; i <= 5; i++) {
          let randomNumber = generate[Math.floor(Math.random() * generate.length)]
          change += randomNumber;
         
